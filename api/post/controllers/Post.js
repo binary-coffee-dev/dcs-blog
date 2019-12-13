ab5e4b7224e0e86cdb48afaa92d388becd1647c9
Guillermo Gonzalez <ggjnez92@gmail.com>
2019-12-13 17:10:33 +0100

    Return list of post for enabled one only (#9)

--- api/post/controllers/Post.js
+++ api/post/controllers/Post.js
@@ -1,7 +1,26 @@
 'use strict';
 
+const {sanitizeEntity} = require('strapi-utils');
+
 /**
  * Read the documentation () to implement custom controller functions
  */
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let entities;
+
+    if (ctx.query._q) {
+      entities = await strapi.services.post.search(ctx.query);
+    } else {
+      entities = await strapi.services.post.find(ctx.query);
+    }
+
+    return entities.reduce((prev, entity) => {
+      if (entity.enable) {
+        prev.push(sanitizeEntity(entity, {model: strapi.models.post}));
+      }
+      return prev;
+    }, []);
+  },
+};
