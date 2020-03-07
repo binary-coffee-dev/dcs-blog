9f643c0422c759a35a929ccae7360a25c03d66d9
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-03-07 02:08:54 +0100

    Create migration for update all the users with the writer role by the staff role (#54)

---
+++ config/functions/db/migrations/1.1.1-replacing_writer_by_staff.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const _ = require('lodash');
+
+module.exports = {
+  version: '1.1.1',
+  description: 'Replacing all users with writer role by the new staff role',
+  migrate: async () => {
+    const writerRole = await strapi.plugins['users-permissions'].models.role.findOne({type: 'writer'});
+    const staffRole = await strapi.plugins['users-permissions'].models.role.findOne({type: 'staff'});
+    if (writerRole) {
+      await strapi.plugins['users-permissions'].models.user.update({role: writerRole.id}, {$set: {role: staffRole}});
+    }
+  }
+};
