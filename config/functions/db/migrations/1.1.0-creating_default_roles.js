2493b67587e5419cfc34ab8c51594cc8db27bac0
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-03-07 03:11:33 +0100

    Add permissions to the default roles (#54)

--- config/functions/db/migrations/1.1.0-creating_default_roles.js
+++ config/functions/db/migrations/1.1.0-creating_default_roles.js
@@ -7,14 +7,27 @@ module.exports = {
   description: 'Create default roles (authenticated|staff|administrator)',
   migrate: async () => {
     const rolesNames = [
-      { type: 'authenticated', description: 'Default role given to authenticated user.'},
-      { type: 'staff', description: 'Users that can review the all the articles'},
-      { type: 'administrator', description: 'Administration control in the application'}
+      {type: 'authenticated', description: 'Default role given to authenticated user.'},
+      {type: 'staff', description: 'Users that can review the all the articles'},
+      {type: 'administrator', description: 'Administration control in the application'}
     ];
+    const publicRole = await strapi.plugins['users-permissions'].models.role.findOne({type: 'public'});
+    const basePermissions = await strapi.plugins['users-permissions'].models.permission.find({role: publicRole.id});
+    const newPermissions = basePermissions.map(permission => ({
+      type: permission.type,
+      controller: permission.controller,
+      action: permission.action,
+      enabled: permission.enabled,
+      policy: permission.policy
+    }));
     for (let {type, description} of rolesNames) {
       const role = await strapi.plugins['users-permissions'].models.role.findOne({type});
       if (!role) {
-        await strapi.plugins['users-permissions'].models.role.create({name: _.capitalize(type), type, description});
+        const newRole = await strapi.plugins['users-permissions'].models.role.create({name: _.capitalize(type), type, description});
+        await strapi.plugins['users-permissions'].models.permission.create(newPermissions.map(permission => ({
+          ...permission,
+          role: newRole.id
+        })));
       }
     }
   }
