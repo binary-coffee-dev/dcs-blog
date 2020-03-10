5a31b4d8ef2cfc59eecdecc276e4a078e56c5d2b
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-03-11 00:29:01 +0100

    Update me method to inject the github avatar url in the user data

---
+++ extensions/users-permissions/controllers/User.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const _ = require('lodash');
+
+const User = require('strapi-plugin-users-permissions/controllers/User');
+
+const UserNew = {
+  ...User,
+  async me(ctx) {
+    await User.me(ctx);
+    if (!ctx.body.avatar) {
+      const FIRST_PROVIDER = 0;
+      const providerId = ctx.state.user.providers[FIRST_PROVIDER];
+      const provider = await strapi.services.provider.findOne({id: providerId});
+      ctx.body.avatar = {
+        url: provider.avatar
+      };
+    }
+  },
+};
+
+module.exports = UserNew;
