4f552e52868d4f16e0c89ae01f2fb64e6a6c3701
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-05-04 17:05:55 +0200

    Integrate policy to the graphql and the api route

--- api/post/config/schema.graphql.js
+++ api/post/config/schema.graphql.js
@@ -16,6 +16,7 @@ module.exports = {
       createPost: {
         resolver: 'Post.create',
         policies: [
+          'global.createPost',
           'global.canModifyPost'
         ]
       },
