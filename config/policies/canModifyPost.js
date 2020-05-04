34331d67aec9a010ea8198258bdf6b628a38fdda
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-05-04 17:05:35 +0200

    Create policy for allow only to staffs and admins update add tags to the post

--- config/policies/canModifyPost.js
+++ config/policies/canModifyPost.js
@@ -5,11 +5,12 @@
  */
 
 module.exports = async (ctx, next) => {
-  if (strapi.services.post.isStaff(ctx)) {
+  if (strapi.services.post.isStaff(ctx) || strapi.services.post.isAdmin(ctx)) {
     return await next();
   } else if (strapi.services.post.isAuthenticated(ctx) && ctx.params.id) {
     const post = await strapi.services.post.findOne({id: ctx.params.id});
     if (Boolean(post.author) && post.author.id === ctx.state.user.id) {
+      delete ctx.request.body.tags;
       return await next();
     }
   }
