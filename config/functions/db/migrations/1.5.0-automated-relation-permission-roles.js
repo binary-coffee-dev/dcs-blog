e37daf6c664d40ac8556a71daa0a97964d890de9
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-06-01 18:45:41 +0200

    Create test to check if the post name is working (#279)

--- config/functions/db/migrations/1.5.0-automated-relation-permission-roles.js
+++ config/functions/db/migrations/1.5.0-automated-relation-permission-roles.js
@@ -8,10 +8,10 @@ module.exports = {
     const staffRole = await strapi.plugins['users-permissions'].models.role.findOne({type: 'staff'});
     const adminRole = await strapi.plugins['users-permissions'].models.role.findOne({type: 'administrator'});
     await strapi.plugins['users-permissions'].models.permission
-      .updateMany({controller: 'post', action: ['find', 'count'], role: writerRole.id}, {$set: {enabled: true}});
+      .updateMany({controller: 'post', action: ['find', 'count', 'create', 'update'], role: writerRole.id}, {$set: {enabled: true}});
     await strapi.plugins['users-permissions'].models.permission
-      .updateMany({controller: 'post', action: ['find', 'count'], role: staffRole.id}, {$set: {enabled: true}});
+      .updateMany({controller: 'post', action: ['find', 'count', 'create', 'update'], role: staffRole.id}, {$set: {enabled: true}});
     await strapi.plugins['users-permissions'].models.permission
-      .updateMany({controller: 'post', action: ['find', 'count'], role: adminRole.id}, {$set: {enabled: true}});
+      .updateMany({controller: 'post', action: ['find', 'count', 'create', 'update', 'delete'], role: adminRole.id}, {$set: {enabled: true}});
   },
 };
---
+++ test/integration/check-post-name.js
@@ -0,0 +1,61 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const createUser = require('../helpers/create-user');
+const deleteUser = require('../helpers/delete-user');
+const deletePost = require('../helpers/delete-post');
+const randomName = require('../helpers/random-name');
+const generateJwt = require('../helpers/generate-jwt-by-user');
+const getPostById = require('../helpers/get-post-by-id');
+
+chai.use(chaiHttp);
+
+const expect = chai.expect;
+
+const MUTATION_CREATE_POST = {
+  operationName: null,
+  query: "mutation ($title: String, $body: String, $description: String, $enable: Boolean, $banner: ID, $author: ID, $tags: [ID], $publishedAt: DateTime) {\n  createPost(input: {data: {publishedAt: $publishedAt, title: $title, body: $body, description: $description, enable: $enable, banner: $banner, author: $author, tags: $tags}}) {\n    post {\n      id\n      __typename\n    }\n    __typename\n  }\n}\n"
+};
+
+describe('Should auto-generate the name of the article after create/update it INTEGRATION', () => {
+  let posts = [];
+
+  let authUser;
+
+  before(async () => {
+    authUser = await createUser({strapi});
+  });
+
+  after(async () => {
+    for (let post of posts) {
+      await deletePost(strapi, post);
+    }
+    await deleteUser(strapi, authUser);
+  });
+
+  it('should create an article with the name attribute', (done) => {
+    const jwt = generateJwt(strapi, authUser);
+    chai.request(strapi.server)
+      .post('/graphql')
+      .set('Authorization', `Bearer ${jwt}`)
+      .send({
+        ...MUTATION_CREATE_POST, variables: {
+          body: "safsadf",
+          description: "sfgsd fg sdfg",
+          title: "this is an example of title",
+          enable: true,
+          tags: ["5eb120d1e7134c0012f4d440"],
+          author: "5deee37e98bbd80013a0a844"
+        }
+      })
+      .end((err, res) => {
+        console.log(res.body.data);
+        getPostById(strapi, res.body.data.createPost.post.id)
+          .then(post => {
+            posts.push(post);
+            expect(post.name).to.equal('this-is-an-example-of-title');
+            done();
+          });
+      });
+  });
+});
