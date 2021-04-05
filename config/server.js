8dbd1b67d7ebc49d448654ed21c3f2c90731aed8
Guillermo Gonzalez <ggjnez92@gmail.com>
2021-04-06 01:43:18 +0200

    Add admin secret default value (#126)

--- config/server.js
+++ config/server.js
@@ -7,7 +7,7 @@ module.exports = ({env}) => ({
   admin: {
     autoOpen: false,
     auth: {
-      secret: env('ADMIN_JWT_SECRET')
+      secret: env('ADMIN_JWT_SECRET', '6V#j7xt3ZHBgBphIiwv2')
     }
   }
 });
