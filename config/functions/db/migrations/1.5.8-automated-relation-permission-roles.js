8cbb5a6eae08b6a4d39dd26d60bd11b557cf4f11
Guillermo Gonzalez <ggjnez92@gmail.com>
2021-04-15 04:57:37 +0200

    Update migration script version (#108)

--- config/functions/db/migrations/1.5.8-automated-relation-permission-roles.js
+++ config/functions/db/migrations/1.5.8-automated-relation-permission-roles.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  version: '1.5.6',
+  version: '1.5.8',
   description: 'Automated relation between permission and roles (comment model)',
   migrate: async () => {
     const writerRole = await strapi.plugins['users-permissions'].models.role.findOne({type: 'authenticated'});
