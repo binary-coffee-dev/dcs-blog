a5df8fcbc9cdf0c54f0039542421c4fef43d85fb
Guillermo <ggjnez92@gmail.com>
2021-09-26 19:56:21 +0200

    Create new link when post is updated and title change (#163)

--- api/post/models/Post.js
+++ api/post/models/Post.js
@@ -6,12 +6,36 @@
 
 module.exports = {
   lifecycles: {
-    beforeCreate: async (model) => {
+    async beforeCreate(model) {
       model.name = strapi.services.post.getNameFromTitle(model.title);
     },
 
     async afterCreate(result) {
       await strapi.models.link.create({name: result.name, post: result._id});
+    },
+
+    async beforeUpdate(params, data) {
+      if (params._id && data.title) {
+        const newName = strapi.services.post.getNameFromTitle(data.title);
+        const titleChange = !(await strapi.services.link.existLinkInPost(newName, params._id));
+        if (titleChange) {
+          data.name = newName;
+        }
+      }
+    },
+
+    async afterUpdate(result, params, data) {
+      if (params._id && data.title) {
+        const newName = strapi.services.post.getNameFromTitle(result.title);
+        const titleChange = !(await strapi.services.link.existLinkInPost(newName, params._id));
+        if (titleChange) {
+          await strapi.models.link.create({name: result.name, post: result._id});
+        }
+      }
+    },
+
+    async afterDelete() {
+      // toDo 26.09.21, guille, remove links
     }
   }
 };
--- test/integration/post/publish-post.js
+++ test/integration/post/publish-post.js
@@ -1,6 +1,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
+const createPost = require('../../helpers/create-post');
 const createUser = require('../../helpers/create-user');
 const deleteUser = require('../../helpers/delete-user');
 const deletePost = require('../../helpers/delete-post');
@@ -44,183 +45,136 @@ describe('Create/Update post with publishedAt attribute INTEGRATION', () => {
     await deleteUser(strapi, adminUser);
   });
 
-  it('should create an article with the publishedAt attribute (admin user)', (done) => {
+  it('should create an article with the publishedAt attribute (admin user)', async () => {
     const jwt = generateJwt(strapi, adminUser);
-    chai.request(strapi.server)
-      .post('/graphql')
-      .set('Authorization', `Bearer ${jwt}`)
-      .send({
-        ...MUTATION_CREATE_POST, variables: {
-          body: 'safsadf',
-          title: 'The good one',
-          enable: true,
-          author: '5deee37e98bbd80013a0a844',
-          publishedAt: new Date()
-        }
-      })
-      .end((err, res) => {
-        getPostById(strapi, res.body.data.createPost.post.id)
-          .then(post => {
-            posts.push(post);
-            expect(!!post.publishedAt).to.equal(true);
-            done();
-          });
-      });
+    const res = await createNewPost({title: 'The good one',}, jwt);
+
+    const post = await getPostById(strapi, res.body.data.createPost.post.id);
+
+    posts.push(post);
+    expect(!!post.publishedAt).to.equal(true);
   });
 
-  it('should create an article without the publishedAt attribute (staff user)', (done) => {
+  it('should create an article without the publishedAt attribute (staff user)', async () => {
     const jwt = generateJwt(strapi, staffUser);
-    chai.request(strapi.server)
-      .post('/graphql')
-      .set('Authorization', `Bearer ${jwt}`)
-      .send({
-        ...MUTATION_CREATE_POST, variables: {
-          body: 'safsadf',
-          title: 'not work',
-          enable: true,
-          author: '5deee37e98bbd80013a0a844',
-          publishedAt: new Date()
-        }
-      })
-      .end((err, res) => {
-        getPostById(strapi, res.body.data.createPost.post.id)
-          .then(post => {
-            posts.push(post);
-            expect(!!post.publishedAt).to.equal(false);
-            done();
-          });
-      });
+    const res = await createNewPost({title: 'not work',}, jwt);
+
+    const post = await getPostById(strapi, res.body.data.createPost.post.id);
+
+    posts.push(post);
+    expect(!!post.publishedAt).to.equal(false);
   });
 
-  it('should create an article without the publishedAt attribute (auth user)', (done) => {
+  it('should create an article without the publishedAt attribute (auth user)', async () => {
     const jwt = generateJwt(strapi, authUser);
-    chai.request(strapi.server)
-      .post('/graphql')
-      .set('Authorization', `Bearer ${jwt}`)
-      .send({
-        ...MUTATION_CREATE_POST, variables: {
-          body: 'safsadf',
-          title: 'not work two',
-          enable: true,
-          author: '5deee37e98bbd80013a0a844',
-          publishedAt: new Date()
-        }
-      })
-      .end((err, res) => {
-        getPostById(strapi, res.body.data.createPost.post.id)
-          .then(post => {
-            posts.push(post);
-            expect(!!post.publishedAt).to.equal(false);
-            done();
-          });
-      });
+    const res = await createNewPost({title: 'not work two',}, jwt);
+
+    const post = await getPostById(strapi, res.body.data.createPost.post.id);
+
+    posts.push(post);
+    expect(!!post.publishedAt).to.equal(false);
   });
 
   it('should update an article with the publishedAt attribute (admin user)', async () => {
-    const adminPost = await strapi.models.post.create({
-      title: randomName(),
-      name: randomName(),
-      body: 'SOME',
-      enable: true,
-      author: authUser
-    });
+    const adminPost = await createPost(strapi, {author: authUser, publishedAt: null});
     posts.push(adminPost);
     const jwt = generateJwt(strapi, adminUser);
-    return await new Promise(resolve => {
-      chai.request(strapi.server)
-        .post('/graphql')
-        .set('Authorization', `Bearer ${jwt}`)
-        .send({
-          ...MUTATION_UPDATE_POST, variables: {
-            id: adminPost.id,
-            enable: true,
-            title: randomName(),
-            body: '# fasdfasd f',
-            publishedAt: new Date(),
-            tags: ['5eb120d1e7134c0012f4d440'],
-            banner: null,
-          }
-        })
-        .end((err, res) => {
-          getPostById(strapi, res.body.data.updatePost.post.id)
-            .then(post => {
-              posts.push(post);
-              expect(!!post.publishedAt).to.equal(true);
-              resolve();
-            });
-        });
-    });
+
+    const res = await updatePost({id: adminPost.id}, jwt);
+    const post = await getPostById(strapi, res.body.data.updatePost.post.id);
+
+    posts.push(post);
+    expect(!!post.publishedAt).to.equal(true);
   });
 
   it('should update an article without the publishedAt attribute (staff user)', async () => {
-    const adminPost = await strapi.models.post.create({
-      title: randomName(),
-      name: randomName(),
-      body: 'SOME',
-      enable: true,
-      author: authUser
-    });
+    const adminPost = await createPost(strapi, {author: authUser, publishedAt: null});
     posts.push(adminPost);
     const jwt = generateJwt(strapi, staffUser);
-    return await new Promise(resolve => {
-      chai.request(strapi.server)
-        .post('/graphql')
-        .set('Authorization', `Bearer ${jwt}`)
-        .send({
-          ...MUTATION_UPDATE_POST, variables: {
-            id: adminPost.id,
-            enable: true,
-            title: randomName(),
-            body: '# fasdfasd f',
-            publishedAt: new Date(),
-            tags: ['5eb120d1e7134c0012f4d440'],
-            banner: null,
-          }
-        })
-        .end((err, res) => {
-          getPostById(strapi, res.body.data.updatePost.post.id)
-            .then(post => {
-              posts.push(post);
-              expect(!!post.publishedAt).to.equal(false);
-              resolve();
-            });
-        });
-    });
+
+    const res = await updatePost({id: adminPost.id}, jwt);
+    const post = await getPostById(strapi, res.body.data.updatePost.post.id);
+
+    posts.push(post);
+    expect(!!post.publishedAt).to.equal(false);
   });
 
   it('should update an article without the publishedAt attribute (auth user)', async () => {
-    const adminPost = await strapi.models.post.create({
-      title: randomName(),
-      name: randomName(),
-      body: 'SOME',
-      enable: true,
-      author: authUser
-    });
+    const adminPost = await createPost(strapi, {author: authUser, publishedAt: null});
     posts.push(adminPost);
     const jwt = generateJwt(strapi, authUser);
-    return await new Promise(resolve => {
-      chai.request(strapi.server)
-        .post('/graphql')
-        .set('Authorization', `Bearer ${jwt}`)
-        .send({
-          ...MUTATION_UPDATE_POST, variables: {
-            id: adminPost.id,
-            enable: true,
-            title: randomName(),
-            body: '# fasdfasd f',
-            publishedAt: new Date(),
-            tags: ['5eb120d1e7134c0012f4d440'],
-            banner: null,
-          }
-        })
-        .end((err, res) => {
-          getPostById(strapi, res.body.data.updatePost.post.id)
-            .then(post => {
-              posts.push(post);
-              expect(!!post.publishedAt).to.equal(false);
-              resolve();
-            });
-        });
-    });
+
+    const res = await updatePost({id: adminPost.id}, jwt);
+    const post = await getPostById(strapi, res.body.data.updatePost.post.id);
+
+    posts.push(post);
+    expect(!!post.publishedAt).to.equal(false);
+  });
+
+  it('should create and then update article and test the created links', async () => {
+    // create post
+    const jwt = generateJwt(strapi, adminUser);
+    let res = await createNewPost({title: 'The good one',}, jwt);
+
+    let post = await getPostById(strapi, res.body.data.createPost.post.id);
+    posts.push(post);
+
+    expect(!!post.publishedAt).to.equal(true);
+
+    // update post without change title
+    res = await updatePost({id: post.id, title: post.title}, jwt);
+    post = await getPostById(strapi, res.body.data.updatePost.post.id);
+
+    let links = await strapi.models.link.find({post: post.id});
+
+    expect(!!post.publishedAt).to.equal(true);
+    expect(links.length).to.equal(1);
+
+    // update with new title
+    res = await updatePost({id: post.id, title: 'this is the new title'}, jwt);
+    post = await getPostById(strapi, res.body.data.updatePost.post.id);
+
+    links = await strapi.models.link.find({post: post.id});
+
+    expect(!!post.publishedAt).to.equal(true);
+    expect(links.length).to.equal(2);
   });
 });
+
+async function updatePost(variables, jwt) {
+  return new Promise((resolve, reject) => {
+    chai.request(strapi.server)
+      .post('/graphql')
+      .set('Authorization', `Bearer ${jwt}`)
+      .send({
+        ...MUTATION_UPDATE_POST, variables: {
+          enable: true,
+          title: randomName(),
+          body: '# fasdfasd f',
+          publishedAt: new Date(),
+          tags: ['5eb120d1e7134c0012f4d440'],
+          banner: null,
+          ...variables
+        }
+      })
+      .end((err, res) => err ? reject(err) : resolve(res));
+  });
+}
+
+function createNewPost(variables, jwt) {
+  return new Promise((resolve, reject) => {
+    chai.request(strapi.server)
+      .post('/graphql')
+      .set('Authorization', `Bearer ${jwt}`)
+      .send({
+        ...MUTATION_CREATE_POST, variables: {
+          body: 'safsadf',
+          enable: true,
+          author: '5deee37e98bbd80013a0a844',
+          publishedAt: new Date(),
+          ...variables
+        }
+      })
+      .end((err, res) => err ? reject(err) : resolve(res));
+  });
+}
