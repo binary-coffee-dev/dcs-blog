81adf44676a19951d721246e00c720ef29700579
Guillermo <ggjnez92@gmail.com>
2023-05-06 16:37:53 +0200

    Update app public url

--- config/server.js
+++ config/server.js
@@ -3,6 +3,7 @@
 module.exports = ({env}) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('API_URL', 'http://localhost:1337'),
   // cron: {
   //   enabled: true,
   //   tasks: cronTasks
