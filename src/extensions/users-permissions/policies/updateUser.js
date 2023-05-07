01913efce4bde48c01999be26185089681f00eb4
Guillermo <ggjnez92@gmail.com>
2023-05-07 20:03:30 +0200

    Fix lint issues

--- src/extensions/users-permissions/policies/updateUser.js
+++ src/extensions/users-permissions/policies/updateUser.js
@@ -2,7 +2,7 @@
 
 const allowedUpdateFields = new Set(['page']);
 
-module.exports = async (ctx, config, {strapi}) => {
+module.exports = async (ctx) => {
   const {data} = ctx.args;
   const keys = Object.keys(data);
   keys.filter(k => !allowedUpdateFields.has(k)).forEach(k => delete data[k]);
