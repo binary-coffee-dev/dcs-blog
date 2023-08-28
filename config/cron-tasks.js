3b9b16d2bd67309f5da5007563c995f2b11e222e
Guillermo Gonzalez <guillermo.gonzalez.ext@syrocon.de>
2023-08-28 09:23:45 +0200

    Update cron task to tests subscription emails (#106)

--- config/cron-tasks.js
+++ config/cron-tasks.js
@@ -1,3 +1,18 @@
+/*
+Cron fromat:
+
+*    *    *    *    *    *
+┬    ┬    ┬    ┬    ┬    ┬
+│    │    │    │    │    |
+│    │    │    │    │    └ day of week (0 - 7) (0 or 7 is Sun)
+│    │    │    │    └───── month (1 - 12)
+│    │    │    └────────── day of month (1 - 31)
+│    │    └─────────────── hour (0 - 23)
+│    └──────────────────── minute (0 - 59)
+└───────────────────────── second (0 - 59, OPTIONAL)
+
+ */
+
 module.exports = {
   subscriptionEmails: {
     task: ({strapi}) => {
@@ -8,7 +23,7 @@ module.exports = {
     options: {
       // every friday
       // rule: "0 18 16 * * 5",
-      rule: '0 18 16 * * *',
+      rule: '0 30 * * * *',
     },
   },
 };
