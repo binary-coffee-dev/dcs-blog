3ed4a73c6239994732869b8b467a404f9c640d4f
guille <ggjnez92@gmail.com>
2025-01-12 17:52:06 +0100

    Update database charset and collation

--- config/database.js
+++ config/database.js
@@ -6,7 +6,9 @@ module.exports = ({env}) => ({
       port: env.int('DATABASE_PORT', 3306),
       database: env('DATABASE_NAME', 'news'),
       user: env('DATABASE_USERNAME', 'api'),
-      password: env('DATABASE_PASSWORD', 'password')
+      password: env('DATABASE_PASSWORD', 'password'),
+      charset: 'utf8mb4',
+      collation: 'utf8mb4_unicode_ci'
     },
     debug: false,
   },
